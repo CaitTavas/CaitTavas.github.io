06a9d009487c6ec9b287badfcdec0ecc15ea31ca
Caitlin <71946359+CaitTavas@users.noreply.github.com>
2024-02-12 00:07:05 -0500

    added footer

---
+++ assets/js/crtfoot.js
@@ -0,0 +1,12 @@
+const footer = d3.select('body').append('footer')
+                                .attr('class', 'mainfooter');
+
+footer.append('h1').text('Contact Information');
+footer.append()
+
+footer.append('a').attr('class', 'fa fa-linkedin')
+        .attr('href', 'https://www.linkedin.com/in/caitlin-tavas')
+        .style('color', 'white')
+        .style('background', "#007bb5");
+
+footer.append()
