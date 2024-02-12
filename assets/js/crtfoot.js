4310c7c0c8a8e1f563ee86d3e83e5fa09c19ce34
Caitlin <71946359+CaitTavas@users.noreply.github.com>
2024-02-12 01:09:44 -0500

    fix link

--- assets/js/crtfoot.js
+++ assets/js/crtfoot.js
@@ -5,7 +5,7 @@ footer.append('h1').text('Contact Information');
 footer.append()
 
 footer.append('a').attr('class', 'fa fa-linkedin')
-        .attr('href', 'https://www.linkedin.com/in/caitlin-tavas')
+        // .attr('href', 'https://www.linkedin.com/in/caitlin-tavas')
         .style('color', 'white')
         .style('background', "#007bb5");
 
